974f2c98a67c26c0f880b49d66f7a6440f0e86f5
thanhan7501 <71816459+thanhan7501@users.noreply.github.com>
2022-04-05 01:17:12 +0700

    add game

--- client/src/pages/admin/game/Game.js
+++ client/src/pages/admin/game/Game.js
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Table } from 'antd';
+import gameApi from '../../../api/game'
 
 const Game = () => {
+  const [gameList, setGameList] = useState([]);
+  const getAllGames = async () => {
+    const allGames = await gameApi.getAllGames();
+    setGameList(allGames.games)
+  }
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      width: 150,
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      width: 150,
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+    },
+  ];
+
+  const data = [];
+  for (let i = 0; i < 100; i++) {
+    data.push({
+      key: i,
+      name: `Edward King ${i}`,
+      age: 32,
+      address: `London, Park Lane no. ${i}`,
+    });
+  }
+
   return (
-    <div>Game</div>
+    <Table columns={columns} dataSource={data} pagination={{ pageSize: 50 }} scroll={{ y: 240 }} />
   )
 }
 
