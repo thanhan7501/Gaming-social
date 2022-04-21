8f0a2ad2829f25736e50c25bb75975e8c0734f0c
thanhan7501 <71816459+thanhan7501@users.noreply.github.com>
2022-04-22 02:05:48 +0700

    lười

--- server/controllers/user/profile.js
+++ server/controllers/user/profile.js
@@ -1,4 +1,5 @@
-const Post = require("../../models/post")
+const Post = require("../../models/post");
+const User = require("../../models/user");
 
 module.exports = {
     getUserPost: async (ctx) => {
@@ -6,12 +7,23 @@ module.exports = {
         const post = await Post.find({ user: userId })
             .populate("user", "-password -createdAt -updatedAt -__v")
             .populate("game", "-createdAt -updatedAt -__v")
-            .lean()
+            .lean();
 
         return (ctx.body = {
             status: true,
             message: "get profile success",
             post,
+        })
+    },
+
+    changeAvatar: async (ctx) => {
+        const { avatarUrl } = ctx.request.body;
+        const userId = ctx.state.user._id;
+        await User.updateOne({ _id: userId}, { avatarUrl: avatarUrl });
+
+        return (ctx.body = {
+            status: true,
+            message: "change avatar success",
         })
     }
 }
--- server/routes/api/user/profile.js
+++ server/routes/api/user/profile.js
@@ -4,4 +4,6 @@ const controller = require("../../../controllers/user/profile")
 
 router.get('/profile', controller.getUserPost);
 
+router.get('/profile/change-avatar', controller.changeAvatar);
+
 module.exports = router.routes();
